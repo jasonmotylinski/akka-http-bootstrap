f2f686859290d0d7cda5c102f279bd2353714770
Jason Motylinski <jasonm@spotify.com>
2017-04-27 18:03:28 -0400

    fix: Blocking web server

--- src/main/scala/WebServer.scala
+++ src/main/scala/WebServer.scala
@@ -28,13 +28,13 @@ object WebServer extends Directives with JsonSupport {
         }
       }
 
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+    val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", 8080)
 
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
-    bindingFuture
+  /*  bindingFuture
       .flatMap(_.unbind()) // trigger unbinding from the port
       .onComplete(_ => system.terminate()) // and shutdown when done
-
+*/
   }
 }
